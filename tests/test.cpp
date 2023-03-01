e7b90a54bf0ae918f663de985dff3691254d5b85
Gianluca Martino <gianluca.martino@tuhh.de>
2023-03-01 20:00:17 +0100

    Adding a basic test.

---
+++ tests/test.cpp
@@ -0,0 +1,40 @@
+#include <vcd-parser/VCDFileParser.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <algorithm>
+#include <cctype>
+#include <locale>
+
+inline void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }));
+}
+
+inline void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }).base(), s.end());
+}
+
+inline void trim(std::string &s) {
+  rtrim(s);
+  ltrim(s);
+}
+
+inline std::string trim_copy(std::string s) {
+  trim(s);
+  return s;
+}
+
+TEST_CASE("Basic parsing", "[VCD]") {
+  VCDFileParser parser;
+
+  auto trace = parser.parse_file("../../tests/testfiles/simple.vcd");
+  CHECK(trace != nullptr);
+  CHECK(trim_copy(trace->version) == "Icarus Verilog");
+  CHECK(trim_copy(trace->date) == "Wed Mar 01 17:08:44 2023");
+  CHECK(trace->get_signals().size() == 14);
+  CHECK(trace->get_timestamps().size() == 11);
+}
