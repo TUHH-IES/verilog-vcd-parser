1f2f669d8a9910b15d78dea10e81ca24c9e8f7a7
SRG2-3 <84575230+SRG2-3@users.noreply.github.com>
2025-10-06 17:48:19 +0200

    Update VCDTypes.hpp
    set VCDTime same as SignalTime i.e. long long

--- include/vcd-parser/VCDTypes.hpp
+++ include/vcd-parser/VCDTypes.hpp
@@ -20,7 +20,7 @@ typedef std::string VCDScopeName;
 typedef std::string VCDSignalHash;
 
 //! Represents a single instant in time in a trace
-typedef long VCDTime;
+typedef long long VCDTime;
 
 //! Specifies the timing resoloution along with VCDTimeUnit
 typedef long VCDTimeRes;
