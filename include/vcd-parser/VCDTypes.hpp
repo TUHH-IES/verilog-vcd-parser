36aa16f998d6fa3c1342e953f90ed3a386b2253c
SRG2-3 <84575230+SRG2-3@users.noreply.github.com>
2025-10-07 10:18:48 +0200

    Update VCDTypes.hpp
    updating VCDTime from long long to int64_t

--- include/vcd-parser/VCDTypes.hpp
+++ include/vcd-parser/VCDTypes.hpp
@@ -20,7 +20,7 @@ typedef std::string VCDScopeName;
 typedef std::string VCDSignalHash;
 
 //! Represents a single instant in time in a trace
-typedef long long VCDTime;
+typedef int64_t VCDTime;
 
 //! Specifies the timing resoloution along with VCDTimeUnit
 typedef long VCDTimeRes;
